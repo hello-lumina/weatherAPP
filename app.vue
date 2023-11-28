4bb760261cc6df74f512126a6db838cc802081c8
TheThorsy <thethorsy@gmail.com>
2023-11-28 13:16:41 +0100

    Dodannie konponentu i sprawdzanie lokalizacji

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    
+  <div id="SearchBar">
+    <Search />
   </div>
 </template>
+
+<script>
+  import Search from '@/components/search.vue';
+
+    export default {
+      components: {
+        Search,
+                  },
+                   };
+</script>
+
+<style>
+/* Opcjonalne: style dla App.vue */
+</style>
---
+++ components/search.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <input placeholder='Wyszukaj lokalizację...' v-model="writingLocation" @click='askForLocationPermission'/>
+        <button @click="search">Szukaj</button>
+            <div v-if="searchLocation != null">
+                <p>Wprowadzona lokalizacja to: {{ searchLocation }}</p>
+            </div>
+    </div>
+</template>
+    
+    <script>
+        export default {
+            data() {
+                return{
+                    searchLocation: null,
+                    writingLocation: '',
+                      };
+                   },
+            methods: {
+                askForLocationPermission() {
+                    if("geolocation" in navigator) {
+                        navigator.geolocation.getCurrentPosition(
+                            (position) => {
+                                console.log('Lokalizacja uzyskana!',position.coords.latitude, position.coords.longitude);
+                                          },
+                            
+                            (error) => {
+                                console.error('Bład!', error);
+                                       },
+                                                                );
+                                                   }
+                    else {
+                        console.error('Przeglądarka nie obsługuje geolokalizacji!');
+                         }
+                                           },
+                search() {
+                        this.searchLocation = this.writingLocation;
+                            },
+                     },
+                       };
+    </script>
+
+        <style scoped>
+            /* Stylizacja komponentu, jeśli jest potrzebna */
+        </style>
