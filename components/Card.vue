be4c640755b9af0e2a8f08f6a9157d6e55cb7f7c
m-malanowski <info@mateusz-malanowski.pl>
2023-12-16 22:57:06 +0100

    LUM-Bartek | dark mode

--- components/Card.vue
+++ components/Card.vue
@@ -1,16 +1,26 @@
 <script lang="js">
+import {getIcon} from "~/components/utils/getIcon.js";
+
 export default {
+  methods: {getIcon},
   props: {
     unit: {
       type: Object,
       required: true
     },
-
     astro: {
       type: Object
     },
     date: {
       type: Date
+    },
+    day: {
+      type: Object
+    },
+
+    setup() {
+
+
     }
   }
 }
@@ -19,13 +29,13 @@ export default {
 <template>
   <div class="card">
     <p>{{ date }}</p>
+
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="60" height="60">
       <path fill="none" d="M0 0h24v24H0z"></path>
-      <path :d="3434"
-            fill="rgb(75,0,130)"></path>
+      <path :d="getIcon(this.day.condition.text)"></path>
     </svg>
 
-    <p>Unit test: {{unit}} <span
+    <p>Unit test: {{ unit }} <span
         class="color-bg-darker"></span></p>
   </div>
 </template>
